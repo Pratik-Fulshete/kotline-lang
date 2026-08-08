c1c9570c5eb3b518b6c4e1a47be0541e40c2b741
Pratik Fulshete <146758547+Pratik-Fulshete@users.noreply.github.com>
2026-08-08 22:12:04 +0530

    user Input

---
+++ readingUserInput.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a = 0;
+    var b = 0;
+    print("Enter number 1 for addition:")
+    a = readln().toInt()
+    println()
+      print("Enter number 2 for addition:")
+    b = readln().toInt()
+
+    println("addition is ${a+b}")
+
+ // for other datatypes also follows same methods readln().typeconversionMethods()   
+} 
